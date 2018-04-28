9af0abfbfe2a2390eb008aa98db8fef3f0420c95
kszysieg <krzysztof.a.kobylinski@gmail.com>
2018-04-28 17:35:37 +0200

    10 - Hold Shift and Check Checkboxes

---
+++ 10 - Hold Shift and Check Checkboxes/script.js
@@ -0,0 +1,28 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(event) {
+
+  let inBetween = false;
+  if (event.shiftKey && this.checked) {
+    checkboxes.forEach(checkbox => {
+      console.log(checkbox);
+      if (checkbox === this || 
+          checkbox === lastChecked) {
+        inBetween = !inBetween;
+        console.log('starting to check them in between!');
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+
+
+  lastChecked = this;
+}
+
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
