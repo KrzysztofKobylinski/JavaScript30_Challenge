0e919e60a355d081ef93d2ad74c6b5378cd84d65
kszysieg <krzysztof.a.kobylinski@gmail.com>
2018-05-07 16:17:45 +0200

    bug fix

--- 26 - Stripe Follow Along Nav/scripts.js
+++ 26 - Stripe Follow Along Nav/scripts.js
@@ -5,9 +5,12 @@ const nav  = document.querySelector('.top');
 function handleEnter() {
     console.log("enter");
     this.classList.add('trigger-enter');
-    setTimeout(() => 
-        this.classList.contains('trigger-enter') && 
-        this.classList.add('trigger-enter-active'), 150);
+    setTimeout(() => {
+        if(this.classList.contains('trigger-enter')) { 
+        this.classList.add('trigger-enter-active')
+        }
+    }
+    , 150);
     background.classList.add('open');
 
 
